d0d90e03cb2433d255cc1c09af36f516a94a2f1a
amoryassine <amor9999amor@gmail.com>
2020-11-14 14:03:15 +0100

    tp1_ 2 commit

--- src/main/java/com/yassine/voiture/service/VoitureService.java
+++ src/main/java/com/yassine/voiture/service/VoitureService.java
@@ -1,5 +1,21 @@
 package com.yassine.voiture.service;
 
+import java.util.List;
+
+import com.yassine.voiture.entities.Voiture;
+
 public interface VoitureService {
+	Voiture updateVoiture(Voiture v);
+	
+	void deleteVoiture(Voiture v);
+	
+	void deleteVoitureById(Long id);
+	
+	Voiture getVoiture(Long id);
+
+	
+	List<Voiture> getAllVoiture();
+	
+
 
 }
