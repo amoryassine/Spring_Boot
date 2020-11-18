55147cd693f4f14c71c5f2fc46c087e4de5f321c
amoryassine <amor9999amor@gmail.com>
2020-11-18 20:09:33 +0100

    repo

--- src/main/java/com/yassine/voiture/repos/VoitureRepository.java
+++ src/main/java/com/yassine/voiture/repos/VoitureRepository.java
@@ -1,9 +1,15 @@
 package com.yassine.voiture.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yassine.voiture.entities.Voiture;
 
 public interface VoitureRepository extends JpaRepository<Voiture, Long> {
+	
+	
+	List<Voiture> findBymarqueVoiture(String marque);
+	List<Voiture> findBymarqueVoitureContains(String marque);
 
 }
